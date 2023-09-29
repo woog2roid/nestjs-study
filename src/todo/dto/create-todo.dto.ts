99c5e7624a121faed1db910306654004db95d48a
woog2roid <woog2roid@gmail.com>
2023-09-29 17:20:36 +0900

    feat: class-validator를 이용한 DTO 생성

--- src/todo/dto/create-todo.dto.ts
+++ src/todo/dto/create-todo.dto.ts
@@ -1,1 +1,16 @@
-export class CreateTodoDto {}
+import { IsString, IsBoolean, MaxLength, MinLength } from 'class-validator';
+
+export class CreateTodoDto {
+  @IsString()
+  @MinLength(1)
+  @MaxLength(100)
+  title: string;
+
+  @IsString()
+  @MinLength(1)
+  @MaxLength(100)
+  description: string;
+
+  @IsBoolean()
+  done: boolean;
+}
