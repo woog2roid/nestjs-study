c1889c22ed7b71601c0083233a3bd7ba9b0f57ed
woog2roid <woog2roid@gmail.com>
2023-09-29 18:38:05 +0900

    setup(bootstrap): swagger, class-validator config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger
+  const document_config = new DocumentBuilder()
+    .setTitle('Blood-Mate Server')
+    .setDescription('Blood-Mate API Description')
+    .setVersion('1.0.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, document_config);
+  SwaggerModule.setup('docs', app, document);
+
+  // Class Validator
+  app.useGlobalPipes(new ValidationPipe());
+
   await app.listen(3000);
 }
 bootstrap();
