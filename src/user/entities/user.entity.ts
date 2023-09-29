12ee1bcd7981254dbedc1db1d92ee5492386557b
woog2roid <woog2roid@gmail.com>
2023-09-29 15:22:17 +0900

    feat: userEntity 생성

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,29 @@
-export class User {}
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({ schema: 'todo-study', name: 'user' })
+export class User {
+  @PrimaryColumn({ name: 'id', type: 'varchar', unique: true, length: 30 })
+  id: string;
+
+  @Column({ name: 'password', type: 'varchar', length: 100, select: false })
+  password: string;
+
+  @Column({ name: 'nickname', type: 'varchar', length: 30 })
+  name: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt: Date | null;
+}
