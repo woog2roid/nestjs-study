c5b6459fbcc4fde094b8dc06e53f5b906d33a3ea
woog2roid <woog2roid@gmail.com>
2023-09-29 14:53:47 +0900

    feat(CLI): nest g res todo

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { TodoModule } from './todo/todo.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, TodoModule],
   controllers: [AppController],
   providers: [AppService],
 })
