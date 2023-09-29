ecd53366d6b26f4f992d4ea45ba749752360385b
woog2roid <woog2roid@gmail.com>
2023-09-29 18:14:06 +0900

    setup(appModule): typeorm, nestjs config

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,32 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { TodoModule } from './todo/todo.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user/entities/user.entity';
+import { Todo } from './todo/entities/todo.entity';
 
 @Module({
-  imports: [UserModule, TodoModule],
+  imports: [
+    ConfigModule.forRoot({ envFilePath: '.env', isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [User, Todo],
+      migrations: [__dirname + '/src/migrations/*.ts'],
+      autoLoadEntities: true,
+      charset: 'utf8mb4',
+      synchronize: process.env.NODE_ENV !== 'production',
+      logging: process.env.NODE_ENV !== 'production',
+      keepConnectionAlive: true,
+    }),
+    UserModule,
+    TodoModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
