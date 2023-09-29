030fdca966085d9e9f51fa773c9d2c06d5ce595c
woog2roid <woog2roid@gmail.com>
2023-09-29 18:39:02 +0900

    feat(/todo/controller): swagger 적용하기

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,32 +1,47 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { TodoService } from './todo.service';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('todo')
 @Controller('todo')
 export class TodoController {
   constructor(private readonly todoService: TodoService) {}
 
+  @ApiOperation({ summary: '할 일 생성하기' })
   @Post()
   create(@Body() createTodoDto: CreateTodoDto) {
     return this.todoService.create(createTodoDto);
   }
 
+  @ApiOperation({ summary: '할 일 목록 조회하기' })
   @Get()
   findAll() {
     return this.todoService.findAll();
   }
 
+  @ApiOperation({ summary: 'id로 할 일 조회하기' })
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.todoService.findOne(+id);
   }
 
+  @ApiOperation({ summary: '할 일 수정하기' })
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateTodoDto: UpdateTodoDto) {
     return this.todoService.update(+id, updateTodoDto);
   }
 
+  @ApiOperation({ summary: '할 일 삭제하기' })
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.todoService.remove(+id);
