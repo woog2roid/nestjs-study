72bd98a34d5de5dc8b235a880955eca9d1081d2b
woog2roid <woog2roid@gmail.com>
2023-09-29 15:24:01 +0900

    feat: todoEntity 생성

--- src/todo/entities/todo.entity.ts
+++ src/todo/entities/todo.entity.ts
@@ -1,1 +1,32 @@
-export class Todo {}
+import {
+  Column,
+  CreateDateColumn,
+  DeleteDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity({ schema: 'todo-study', name: 'todo' })
+export class Todo {
+  @PrimaryGeneratedColumn({ name: 'id', type: 'int' })
+  id: number;
+
+  @Column({ name: 'title', type: 'varchar', length: 100 })
+  title: string;
+
+  @Column({ name: 'description', type: 'varchar', length: 100 })
+  description: string;
+
+  @Column({ name: 'is_done', type: 'boolean', default: false })
+  isDone: boolean;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @DeleteDateColumn()
+  deletedAt: Date | null;
+}
