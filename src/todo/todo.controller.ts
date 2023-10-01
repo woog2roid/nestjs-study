c41e535fd20e9886f79471f9e948b3b68f5dcc91
woog2roid <woog2roid@gmail.com>
2023-10-02 03:47:41 +0900

    feat: 에러 처리 및 return Type 명시

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -10,7 +10,8 @@ import {
 import { TodoService } from './todo.service';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { Todo } from './entities/todo.entity';
 
 @ApiTags('todo')
 @Controller('todo')
@@ -19,31 +20,34 @@ export class TodoController {
 
   @ApiOperation({ summary: '할 일 생성하기' })
   @Post()
-  create(@Body() createTodoDto: CreateTodoDto) {
+  create(@Body() createTodoDto: CreateTodoDto): Promise<Todo> {
     return this.todoService.create(createTodoDto);
   }
 
   @ApiOperation({ summary: '할 일 목록 조회하기' })
   @Get()
-  findAll() {
+  findAll(): Promise<Todo[]> {
     return this.todoService.findAll();
   }
 
   @ApiOperation({ summary: 'id로 할 일 조회하기' })
   @Get(':id')
-  findById(@Param('id') id: string) {
+  findById(@Param('id') id: string): Promise<Todo> {
     return this.todoService.findById(+id);
   }
 
   @ApiOperation({ summary: '할 일 수정하기' })
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateTodoDto: UpdateTodoDto) {
+  update(
+    @Param('id') id: string,
+    @Body() updateTodoDto: UpdateTodoDto,
+  ): Promise<Todo> {
     return this.todoService.update(+id, updateTodoDto);
   }
 
   @ApiOperation({ summary: '할 일 삭제하기' })
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.todoService.remove(+id);
+  remove(@Param('id') id: string): Promise<void> {
+    return this.todoService.deleteById(+id);
   }
 }
